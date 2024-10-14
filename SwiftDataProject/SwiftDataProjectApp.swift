85c90de338408fac0365096d691065780c8497dd
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-10-14 14:33:30 +0300

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Mykola Chaikovskyi on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
